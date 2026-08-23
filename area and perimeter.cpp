f7217ba0c27fd959164caa302547d49580b6b779
AnvishaBisht <Anvisha.33874@stu.upes.ac.in>
2026-08-23 10:39:19 +0530

    Add files via upload

---
+++ area and perimeter.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area, perimeter;
+
+    printf("Enter the length: ");
+    scanf("%f", &length);
+
+    printf("Enter the breadth: ");
+    scanf("%f", &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
