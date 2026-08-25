c04601cd69fa84e5d328c0060fde2f8263122444
Anvisha <Anvisha.33874@stu.upes.ac.in>
2026-08-25 10:18:12 +0530

    Add files via upload

---
+++ write a program to check if a number is a palindrome.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (originalNum == reverse) {
+        printf("%d is a Palindrome number.", originalNum);
+    } else {
+        printf("%d is not a Palindrome number.", originalNum);
+    }
+
+    return 0;
+}
