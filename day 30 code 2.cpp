a2727c9c766f91e255ee674402ab7e203ca8d51e
Anvisha <Anvisha.33874@stu.upes.ac.in>
2026-09-08 18:59:43 +0530

    Add files via upload

---
+++ day 30 code 2.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int arr[100];
+    int positive = 0, negative = 0, zero = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("\nPositive elements = %d", positive);
+    printf("\nNegative elements = %d", negative);
+    printf("\nZero elements = %d", zero);
+
+    return 0;
+}
