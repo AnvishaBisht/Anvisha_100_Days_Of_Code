b052fde8db3364b86ec47de2b35c146cc30b9814
Anvisha <Anvisha.33874@stu.upes.ac.in>
2026-09-09 19:12:19 +0530

    Add files via upload

---
+++ day 31 code 1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, key, found = 0;
+
+    // Input array size
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input element to search
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    // Linear Search
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            found = 1;
+            printf("Element found at position %d\n", i + 1);
+            break;
+        }
+    }
+
+    if(found == 0) {
+        printf("Element not found.\n");
+    }
+
+    return 0;
+}
