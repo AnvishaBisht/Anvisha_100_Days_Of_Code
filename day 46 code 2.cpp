9933aa34cc0cefeeed896d861590367a85074926
Anvisha <Anvisha.33874@stu.upes.ac.in>
2026-09-24 19:43:46 +0530

    Add files via upload

---
+++ day 46 code 2.cpp
@@ -0,0 +1,37 @@
+//find the first  repeating lowercase alphabet in a string 
+
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j;
+    int found = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            for (j = i + 1; str[j] != '\0'; j++) {
+                if (str[i] == str[j]) {
+                    printf("First repeating lowercase alphabet: %c", str[i]);
+                    found = 1;
+                    break;
+                }
+            }
+        }
+
+        if (found)
+            break;
+    }
+
+    if (!found)
+        printf("No repeating lowercase alphabet found.");
+
+    return 0;
+}
